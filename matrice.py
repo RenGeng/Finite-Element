9c2f7ae695adce530190625b90b92c43aeeef319
Geng <geng2608@hotmail.com>
2019-01-19 18:45:09 +0100

    Add coo matrix format

--- matrice.py
+++ matrice.py
@@ -3,6 +3,7 @@
 
 from read_msh import read_msh
 import numpy as np
+from scipy.sparse import coo_matrix,csr_matrix
 
 list_element = []
 
@@ -23,7 +24,9 @@ class Solveur:
 		return self.list_element[triangle-1].list_index[sommet] # -1 car on commence à 0
 
 	def matriceMasse(self):
-		self.M = np.zeros((self.nb_point,self.nb_point)) # Matrice de masse # à faire au format COO
+		data = []
+		ind_ligne = []
+		ind_col = []
 
 		for p in range(self.nb_noTriangle+1,self.nb_element+1):
 			p1 = self.list_point[self.loc2glob(p,0)-1] # -1 car on commence à 0
@@ -38,20 +41,24 @@ class Solveur:
 					J = self.loc2glob(p,j) - 1
 
 					if I == J:
-						self.M[I,J] += det_jaccob/12.0 # car 2 fois l'aire
+						data.append(det_jaccob/12.0)
 					else:
-						self.M[I,J] += det_jaccob/24.0
+						data.append(det_jaccob/24.0)
+					ind_ligne.append(I)
+					ind_col.append(J)
 
+		self.M = coo_matrix((data, (ind_ligne,ind_col))).tocsr()
 
 		# Vérification
-
-		# Matrice de masse
 		U = np.ones((self.nb_point,1))
 		test = self.M.dot(U)
 		print(sum(test))
 
 	def matriceRigidite(self):
-		self.D = np.zeros((self.nb_point,self.nb_point)) # Matrice de rigidité # à faire au format COO
+		# self.D = np.zeros((self.nb_point,self.nb_point)) # Matrice de rigidité # à faire au format COO
+		data = []
+		ind_ligne = []
+		ind_col = []
 
 		grad_phi = [np.array([[-1,-1]]), np.array([[1,0]]), np.array([[0,1]])] # gradient de phi dans le triangle de ref
 
@@ -69,12 +76,17 @@ class Solveur:
 				I = self.loc2glob(p,i) - 1
 				for j in range(3):
 					J = self.loc2glob(p,j) - 1
+
+					ind_ligne.append(I)
+					ind_col.append(J)
+
 					d_temp = grad_phi[j].dot(B_rigidite)
-					self.D[I,J] += det_jaccob/2.0 * d_temp.dot(np.transpose(grad_phi[i]))
-
+					# D[I,J] += det_jaccob/2.0 * d_temp.dot(np.transpose(grad_phi[i]))
+					# print det_jaccob/2.0 * d_temp.dot(np.transpose(grad_phi[i]))
+					data.append((det_jaccob/2.0 * d_temp.dot(np.transpose(grad_phi[i])))[0][0]) # [0][0] car dot donne une liste de liste avec seulement une valeur
+					
+		self.D = coo_matrix((data, (ind_ligne,ind_col))).tocsr()
 		# Vérification
-
-			# Matrice de rigidité
 		U = np.ones((self.nb_point,1))
 		print(sum(self.D.dot(U)))
 
@@ -104,3 +116,4 @@ class Solveur:
 	def u_inc(self,x,y):
 		alpha = 1
 		return np.exp(np.complex(0,2*np.pi)*(x*np.cos(alpha) + y*np.sin(alpha)))
+
