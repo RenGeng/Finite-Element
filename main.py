dc387fd340f831d8fe3544729caba49da6a0baa4
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2019-01-24 00:26:25 +0100

    Fourier Robin cond

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
 
 	test.matriceMasse()
 	test.matriceRigidite()
+	test.matriceRobin()
 	test.assemblage()
--- matrice.py
+++ matrice.py
@@ -53,6 +53,7 @@ class Solveur:
 		U = np.ones((self.nb_point,1))
 		test = self.M.dot(U)
 		print(sum(test))
+		print("Shape:",self.M.shape)
 
 	def matriceRigidite(self):
 		# self.D = np.zeros((self.nb_point,self.nb_point)) # Matrice de rigidité # à faire au format COO
@@ -86,16 +87,54 @@ class Solveur:
 					data.append((det_jaccob/2.0 * d_temp.dot(np.transpose(grad_phi[i])))[0][0]) # [0][0] car dot donne une liste de liste avec seulement une valeur
 					
 		self.D = coo_matrix((data, (ind_ligne,ind_col))).tocsr()
+		print("Shape:",self.D.shape)
 		# Vérification
-		U = np.ones((self.nb_point,1))
-		print(sum(self.D.dot(U)))
+#		U = np.ones((self.nb_point,1))
+#		print(sum(self.D.dot(U)))
+
+
+	def matriceRobin(self):
+		"""
+		Matrice pour la condition au bord de type Robin-Fourier.
+		"""
+
+		data = []
+		ind_ligne = []
+		ind_col = []
+		for p in range(0,self.nb_noTriangle+1):
+			#print("Robin")
+			if self.list_element[p-1].physical == 2: # bord de l
+				p1 = self.list_point[self.loc2glob(p,0)-1] # -1 car on commence à 0
+				p2 = self.list_point[self.loc2glob(p,1)-1]
+				#p3 = self.list_point[self.loc2glob(p,2)-1]
+				#print("type",self.list_element[p-1].physical)
+				#print("p",p,":",p1,p2,p3)
+
+				#sigma =[p1,p2]
+				sigma = np.linalg.norm((p1[0]-p2[0],p1[1]-p2[1]))
+
+				for i in range(2):
+					I = self.loc2glob(p,i) - 1
+					for j in range(2):
+						J = self.loc2glob(p,j) - 1
+
+						if I == J:
+							data.append(sigma/3.0)
+						else:
+							data.append(sigma/6.0)
+						ind_ligne.append(I)
+						ind_col.append(J)
+
+		self.Mbord = coo_matrix((data, (ind_ligne,ind_col)),shape=(self.nb_point, self.nb_point)).tocsr()
+		print("Mbord",self.Mbord.todense())
+		print("Shape:",self.Mbord.shape)
 
 
 	def assemblage(self):
 		self.matriceMasse()
 		self.matriceRigidite()
 
-		A = self.M + self.D
+		A = self.M + self.D + self.Mbord
 		self.B = np.zeros(self.nb_point,dtype=complex)
 
 		print(self.list_point[0])
@@ -110,8 +149,8 @@ class Solveur:
 					A[point-1,point-1] = 1
 					self.B[point-1] = self.u_inc(self.list_point[point-1][0], self.list_point[point-1][1])
 
-		np.savetxt("test.csv",A)
-		# print(A)
+		np.savetxt("test.data",A.todense(),fmt='%.8f')
+		print(A.shape)
 		
 	def u_inc(self,x,y):
 		alpha = 1
